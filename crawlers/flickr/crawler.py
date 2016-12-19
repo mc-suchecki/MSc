2f96bb5202b6cf2a1ebc7191b91b0e9383e7ac49
mc-suchecki <mc.suchecki@gmail.com>
2016-12-19 14:04:22 +0100

    Minor changes in Flickr crawler

--- crawlers/flickr/crawler.py
+++ crawlers/flickr/crawler.py
@@ -1,11 +1,11 @@
 import datetime
 import flickrapi
-import random
 import requests
 
 # TODO handle duplicates
 # TODO better way to avoid pictures not being photos
 # TODO what to do about the aspect ratio?
+# TODO number of favorites is not a very good approximation of aesthetics of the photo
 
 # start measuring time
 start_time = datetime.datetime.now()
@@ -32,10 +32,10 @@ flickr = flickrapi.FlickrAPI(api_key, api_secret)
 # to make the results more random, we select random months and download the pictures
 number_of_downloaded_photos = 0
 min_upload_date = datetime.datetime(2012, 1, 1)
-#while number_of_downloaded_photos < NUMBER_OF_PHOTOS_TO_GET:
+# while number_of_downloaded_photos < NUMBER_OF_PHOTOS_TO_GET:
 while min_upload_date <= datetime.datetime.now():
-    #random_year = random.randint(2010, 2016)
-    #random_month = random.randint(1, 12)
+    # random_year = random.randint(2010, 2016)
+    # random_month = random.randint(1, 12)
     max_upload_date = min_upload_date + datetime.timedelta(days=30)
     result = flickr.photos.search(api_key=api_key, min_upload_date=min_upload_date, max_upload_date=max_upload_date,
                                   media='photos', content_type=1, tags=TAGS, per_page=PAGE_SIZE, tag_mode='any',
@@ -51,7 +51,7 @@ while min_upload_date <= datetime.datetime.now():
         url = 'https://farm{}.staticflickr.com/{}/{}_{}_{}.jpg'.format(farm_id, server_id, photo_id, secret, PHOTO_SIZE)
         fav_result = flickr.photos.getFavorites(api_key=api_key, photo_id=photo_id, format='parsed-json')
         stars = fav_result['photo']['total']
-        # print('Downloading from ' + url + '... (' + stars + ' stars photo)')
+        print('Downloading from ' + url + '... (' + stars + ' stars photo)')
         file_name = DOWNLOAD_LOCATION + stars + '_' + str(farm_id) + '_' + server_id + '_' + photo_id + '_' + secret + \
                     '.jpg'
         with open(file_name, 'wb') as file:
