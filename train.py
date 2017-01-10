b608917dc6b41390b7dabbc6ec6e52c7a05f254e
mc-suchecki <mc.suchecki@gmail.com>
2017-01-10 14:43:14 +0100

    First - extremely naive - version of the neural net with only one neuron - fixed loading of data (wrong parameter passed to slice_input_producer) - fixed displaying of sample images in TensorBoard (commented out so far) - added accuracy calculation on the validation set (very sloppy so far) - minor improvements

--- train.py
+++ train.py
@@ -13,10 +13,11 @@ VALIDATION_IMAGES_DIR = SOURCE_DIR + 'validation/'
 TEST_IMAGES_DIR = SOURCE_DIR + 'test/'
 LIST_FILE_NAME = 'list.txt'
 BATCH_SIZE = 100
-NUMBER_OF_EPOCHS = 1
-TRAINING_SET_SIZE = 15873                                # TODO read this from file?
+NUMBER_OF_EPOCHS = 1                                      # TODO check this
+TRAINING_SET_SIZE = 15873                                 # TODO read this from file?
 
 
+# TODO take batch size as a parameter
 def create_photo_and_label_batches(source_directory):
   """TODO comment, split and throw to another file."""
   # read the list of photo IDs and labels
@@ -32,14 +33,13 @@ def create_photo_and_label_batches(source_directory):
   filenames = tf.convert_to_tensor(filenames_list, dtype=tf.string)
   labels = tf.convert_to_tensor(labels_list, dtype=tf.bool)
   # create queue with filenames and labels
-  file_names_queue, labels_queue = tf.train.slice_input_producer([filenames, labels], num_epochs=NUMBER_OF_EPOCHS,
-                                                                 shuffle=True)
+  file_names_queue, labels_queue = tf.train.slice_input_producer([filenames, labels], shuffle=True)
   # convert filenames of photos to input vectors
   photos_queue = tf.read_file(file_names_queue)                                     # open the file with the photo
   photos_queue = tf.image.decode_jpeg(photos_queue, channels=NUMBER_OF_CHANNELS)    # convert file to numbers
   photos_queue.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, NUMBER_OF_CHANNELS])           # tell TF the size of images
   photos_queue = tf.to_float(photos_queue)                                          # convert uint8 to float32
-  photos_queue = tf.reshape(photos_queue, [-1])                                     # flatten the tensor
+  photos_queue = tf.reshape(photos_queue, [-1])                                     # flatten the tensor (TODO remove)
   # slice the data into mini batches
   return tf.train.batch([photos_queue, labels_queue], batch_size=BATCH_SIZE)
 
@@ -48,55 +48,58 @@ def main(_):
   # start measuring time
   start_time = datetime.datetime.now()
 
-  # load the training set
+  # load the datasets
   training_photo_batch, training_label_batch = create_photo_and_label_batches(TRAINING_IMAGES_DIR)
+  validation_photo_batch, validation_label_batch = create_photo_and_label_batches(VALIDATION_IMAGES_DIR)
+  # test_photo_batch, test_label_batch = create_photo_and_label_batches(TEST_IMAGES_DIR)
 
   # create the model
-  # x = tf.placeholder(tf.float32, [None, 240*159*3])       # input tensor
-  x = training_photo_batch
-  W = tf.Variable(tf.zeros([IMAGE_WIDTH * IMAGE_HEIGHT * NUMBER_OF_CHANNELS, 1], dtype=tf.float32))  # weights tensor
-  # b = tf.Variable(tf.zeros([1], dtype=tf.float32))  # bias
+  x = training_photo_batch                                                        # input tensor (so far flattened)
+  w = tf.Variable(tf.zeros([TRAINING_EXAMPLE_SIZE, 1], dtype=tf.float32))         # weights tensor
+  b = tf.Variable(tf.zeros([1], dtype=tf.float32))                                # bias tensor (so far only 1 neuron)
+  y_ = training_label_batch                                                       # labels tensor
+  y = tf.sigmoid(tf.matmul(x, w) + b)                                             # predictions
 
-  print(x.get_shape())
-  print(W.get_shape())
-  # print(b.get_shape())
-  # y_ = tf.placeholder(tf.float32, [None, 10])
-  y_ = training_label_batch
-  print(y_.get_shape())
-  # y = tf.matmul(x, W) + b
-  y = tf.matmul(x, W)
-  print(y.get_shape())
-
-  # define loss and optimizer
+  # training
   cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+  # evaluating
+  y_validation = tf.sigmoid(tf.matmul(validation_photo_batch, w) + b)
+  correct_prediction = tf.equal(tf.cast(tf.round(y_validation), tf.bool), validation_label_batch)
+  validation_accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
   # initialize session
   sess = tf.InteractiveSession()
 
   # initialize TensorBoard
-  tf.summary.image('Input images', x)
+  # with tf.name_scope('input'):
+  #   tf.summary.image('images', x, 20)
+  with tf.name_scope('evaluation'):
+    tf.summary.scalar('accuracy', validation_accuracy)
   summaries = tf.summary.merge_all()
-  train_writer = tf.summary.FileWriter('/tmp/tf', sess.graph)
+  train_writer = tf.summary.FileWriter('/tmp/tf/train', sess.graph)
+  validation_writer = tf.summary.FileWriter('/tmp/tf/validation', sess.graph)
 
   # do the training
   tf.initialize_all_variables().run()
   coord = tf.train.Coordinator()
   threads = tf.train.start_queue_runners(coord=coord)
-  for i in range(TRAINING_SET_SIZE // BATCH_SIZE):
-    print('Loading next ' + str(BATCH_SIZE) + ' photos...')
-    print('Training batch ' + str(i) + '/' + str(TRAINING_SET_SIZE // BATCH_SIZE) + '...')
+  number_of_batches = TRAINING_SET_SIZE // BATCH_SIZE
+  for i in range(number_of_batches):
+    print('Training with batch ' + str(i) + '/' + str(number_of_batches) + ' (' + str(BATCH_SIZE) + ' photos)...')
+    # summary, _ = sess.run([summaries, train_step])
     sess.run(train_step)
-    sess.run(summaries)
+    # train_writer.add_summary(summary, i)
+    print('Evaluating the model on validation set...')
+    # TODO this is evaluating only 100 images from the validation set!
+    validation_accuracy_summary = sess.run(summaries)
+    validation_writer.add_summary(validation_accuracy_summary, i)
 
   # test trained model
   print('Done! The whole training took ' + str((datetime.datetime.now() - start_time).seconds) + ' seconds.')
-  # print('Evaluating the resulting model accuracy...')
-  # correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-  # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  # test_photo_batch, test_label_batch = create_photo_and_label_batches(TEST_IMAGES_DIR)
-  # test_label_batch = create_photo_and_label_batches(TEST_IMAGES_DIR + LIST_FILE_NAME)
-  # print(sess.run(accuracy, feed_dict={x: test_photo_batch, y_: test_label_batch}))
+
+  # TODO evaluate the model on the test set
 
   # stop our queue threads and properly close the session
   coord.request_stop()
