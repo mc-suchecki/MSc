1477fb3968453937342f1a2e86822f03bd089685
mc-suchecki <mc.suchecki@gmail.com>
2017-01-30 18:16:00 +0100

    Modified the way to calculate the statistics on resolutions of downloaded photos

--- scripts/analyze_resolutions.py
+++ scripts/analyze_resolutions.py
@@ -1,39 +1,27 @@
 """Displays a resolution breakdown - how many photos have a given resolution."""
-from PIL import Image
 from sortedcontainers import SortedDict
-import os
 import pylab
 
 # settings
-DIRECTORY = '../data/'
-VALID_EXTENSIONS = ('.JPG', '.jpg', '.jpeg')
-THRESHOLD = 100
-
-
-def is_photo(file_name):
-  return file_name.endswith(VALID_EXTENSIONS)
-
-
-def get_photo_dimensions(photo_path):
-  image = Image.open(DIRECTORY + photo_path)
-  image.verify()
-  return image.size
-
+SOURCE_DIRECTORY = '../data/'
+PHOTOS_LIST_FILE = SOURCE_DIRECTORY + 'list.txt'
+THRESHOLD = 2000
 
 print('Calculating statistics...')
 occurrences = SortedDict()
-for subdir, dirs, files in os.walk(DIRECTORY):
-  for file in files:
-    if is_photo(file):
-      width, height = get_photo_dimensions(file)
-      # round the numbers to nearest 20s (we will crop the photos to have more for training)
-      width = int(pylab.math.ceil(width / 20.0)) * 20
-      height = int(pylab.math.ceil(height / 20.0)) * 20
-      resolution_string = str(width) + 'x' + str(height)
-      if resolution_string in occurrences:
-        occurrences[resolution_string] += 1
-      else:
-        occurrences[resolution_string] = 0
+with open(PHOTOS_LIST_FILE) as photos_list_file:
+  photos_list = photos_list_file.readlines()
+  for photo_metadata in photos_list:
+    width = int(photo_metadata.split(',')[3])
+    height = int(photo_metadata.split(',')[4])
+    # round the numbers to nearest 20s (we will crop the photos to have more for training)
+    # width = int(pylab.math.ceil(width / 20.0)) * 20
+    # height = int(pylab.math.ceil(height / 20.0)) * 20
+    resolution_string = str(width) + 'x' + str(height)
+    if resolution_string in occurrences:
+      occurrences[resolution_string] += 1
+    else:
+      occurrences[resolution_string] = 0
 
 print('Removing values lower than threshold...')
 for key, value in list(occurrences.items()):
