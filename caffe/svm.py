003f710487193ce6797927f6fef012a70aff6f91
mc-suchecki <mc.suchecki@gmail.com>
2017-02-16 19:36:19 +0100

    Modified Caffe script to take random image from the dataset and display output

--- caffe/svm.py
+++ caffe/svm.py
@@ -1,14 +1,19 @@
 import os
 import sys
 import numpy
+import random
 import matplotlib.pyplot
 
-caffe_root = '../../../caffe/'
-sys.path.insert(0, caffe_root + 'python/')
+CAFFE_ROOT = '../../../caffe/'
+sys.path.insert(0, CAFFE_ROOT + 'python/')
 import caffe
 
-# if os.path.isfile(caffe_root + 'models/finetune_flickr_style/finetune_flickr_style.caffemodel'):
-if os.path.isfile(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'):
+# settings
+PHOTOS_LOCATION = '../data/train/'
+PHOTOS_LIST_LOCATION = PHOTOS_LOCATION + 'list.txt'
+MEAN_FILE_LOCATION = PHOTOS_LOCATION + 'mean.npy'
+
+if os.path.isfile(CAFFE_ROOT + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'):
   print('Caffe model found.')
 else:
   print('Caffe model not found.')
@@ -16,19 +21,16 @@ else:
 caffe.set_device(0)
 caffe.set_mode_gpu()
 
-# model_def = caffe_root + 'models/finetune_flickr_style/deploy.prototxt'
-# model_weights = caffe_root + 'models/finetune_flickr_style/finetune_flickr_style.caffemodel'
-model_def = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
-model_weights = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+model_def = CAFFE_ROOT + 'models/bvlc_reference_caffenet/deploy.prototxt'
+model_weights = CAFFE_ROOT + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
 
 net = caffe.Net(model_def,  # defines the structure of the model
                 model_weights,  # contains the trained weights
                 caffe.TEST)  # use test mode (e.g., don't perform dropout)
 
 # load the mean ImageNet image (as distributed with Caffe) for subtraction
-mu = numpy.load('../data/train/mean.npy')
-mu = numpy.swapaxes(mu, 0, 2)
-# mu2 = numpy.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy')
+mu = numpy.load(MEAN_FILE_LOCATION)
+mu = numpy.swapaxes(mu, 0, 2)  # Caffe expects the mean to be in shape (CHANNELS x WIDTH x HEIGHT)
 mu = mu.mean(1).mean(1)  # average over pixels to obtain the mean (BGR) pixel values
 print('Mean-subtracted values:', list(zip('BGR', mu)))
 
@@ -45,24 +47,32 @@ net.blobs['data'].reshape(50,  # batch size
                           227, 227)  # image size is 240x180
 
 # load the image TODO put that in a loop later
-image = caffe.io.load_image('../data/train/7225053870.jpg')
+print('Getting a random photo from dataset...')
+photos_list = open(PHOTOS_LIST_LOCATION, 'r').read().splitlines()
+random_photo_location = PHOTOS_LOCATION + random.choice(photos_list).split(',')[0] + '.jpg'
+print('Loading photo...')
+image = caffe.io.load_image(random_photo_location)
 transformed_image = transformer.preprocess('data', image)
 
 # copy the image data into the memory allocated for the net
 net.blobs['data'].data[...] = transformed_image
 
 # perform classification
+print('Classifying...')
 output = net.forward()
 
 # interpret the output
 output_prob = output['prob'][0]  # the output probability vector for the first image in the batch
 print('Predicted class is:', output_prob.argmax())
-labels_file = caffe_root + 'data/ilsvrc12/synset_words.txt' # load ImageNet labels
+labels_file = CAFFE_ROOT + 'data/ilsvrc12/synset_words.txt'  # load ImageNet labels
 labels = numpy.loadtxt(labels_file, str, delimiter='\t')
 print('Output label:', labels[output_prob.argmax()])
 top_inds = output_prob.argsort()[::-1][:5]  # reverse sort and take five largest items
 print('Probabilities and labels:')
 print(list(zip(output_prob[top_inds], labels[top_inds])))
+
+# print the activations of last fully connected layer TODO pass this to the SVM
+# print(net.blobs['fc8'].data[0])
 
 # show the image
 matplotlib.pyplot.imshow(image)
