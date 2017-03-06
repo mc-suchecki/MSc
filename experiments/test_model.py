9f1b348b26d82be01ffa084eb72649c02d1c2311
mc-suchecki <mc.suchecki@gmail.com>
2017-03-06 19:24:16 +0100

    Changed testing script to use classification report and confusion matrix instead of just accuracy for reporting the classifier performance

--- experiments/test_model.py
+++ experiments/test_model.py
@@ -1,20 +1,27 @@
 import numpy
 from sklearn.externals import joblib
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 # settings
 TEST_DATA_LOCATION = '../data/test/'
-EXAMPLES_FILE_NAME = 'X.npy'
-LABELS_FILE_NAME = 'y.npy'
+EXAMPLES_FILE_NAME = 'X_fc6_cut.npy'
+LABELS_FILE_NAME = 'y_fc6_cut.npy'
 
 # loading
-print("Loading the model...")
-model = joblib.load('./random_forests_model.pkl')
+print('Loading the classifier...')
+classifier = joblib.load('./results/rf_fc6_4096_model.pkl')
 
 # testing
-print("Loading test data...")
-X = numpy.load(TEST_DATA_LOCATION + EXAMPLES_FILE_NAME)
-y = numpy.load(TEST_DATA_LOCATION + LABELS_FILE_NAME)
-print("X shape is {}, y shape is {}".format(X.shape, y.shape))
-print("Testing the SVM classifier...")
-score = model.score(X, y)
-print("Mean accuracy on the test set is {}.".format(score))
+print('Loading test data...')
+X_test = numpy.load(TEST_DATA_LOCATION + EXAMPLES_FILE_NAME)
+y_test = numpy.load(TEST_DATA_LOCATION + LABELS_FILE_NAME)
+print('X shape is {}, y shape is {}'.format(X_test.shape, y_test.shape))
+print('Testing the classifier...')
+predictions = classifier.predict(X_test)
+print('Classification report on the test set:')
+print(classification_report(y_test, predictions))
+print()
+print('Confusion matrix for the test set:')
+print(confusion_matrix(y_test, predictions))
+print()
