70321fd19d3cce8d5b27071b2739f239626bdd9f
mc-suchecki <mc.suchecki@gmail.com>
2017-03-07 10:55:17 +0100

    Minor improvements to the script that cuts the dataset for scikit-learn

--- experiments/cut_dataset.py
+++ experiments/cut_dataset.py
@@ -1,13 +1,14 @@
 import numpy
 
+FILES_EXTENSION = '.npy'
 TRAIN_DATA_LOCATION = '../data/train/'
 TEST_DATA_LOCATION = '../data/test/'
-EXAMPLES_FILE_NAME = 'X.npy'
-LABELS_FILE_NAME = 'y.npy'
-CUT_EXAMPLES_FILE_NAME = 'X_cut.npy'
-CUT_LABELS_FILE_NAME = 'y_cut.npy'
-TRAINING_SET_SIZE = 2**17
-TEST_SET_SIZE = 2**15
+EXAMPLES_FILE_NAME = 'X_fc6_no_relu'
+LABELS_FILE_NAME = 'y_fc6_no_relu'
+TRAINING_SET_SIZE = 2 ** 17
+TEST_SET_SIZE = 2 ** 15
+TRAINING_SET_SUFFIX = '_first_' + str(TRAINING_SET_SIZE) + FILES_EXTENSION
+TEST_SET_SUFFIX = '_first_' + str(TEST_SET_SIZE) + FILES_EXTENSION
 
 print('Loading training data...')
 X_train = numpy.load(TRAIN_DATA_LOCATION + EXAMPLES_FILE_NAME)
@@ -20,8 +21,10 @@ y_train = y_train[:TRAINING_SET_SIZE:1]
 print('Training data: X shape is {}, y shape is {}'.format(X_train.shape, y_train.shape))
 
 print('Saving training data...')
-numpy.save(TRAIN_DATA_LOCATION + CUT_EXAMPLES_FILE_NAME, X_train)
-numpy.save(TRAIN_DATA_LOCATION + CUT_LABELS_FILE_NAME, y_train)
+numpy.save(TRAIN_DATA_LOCATION + EXAMPLES_FILE_NAME + TRAINING_SET_SUFFIX, X_train)
+print('Training examples saved to:' + TRAIN_DATA_LOCATION + EXAMPLES_FILE_NAME + TRAINING_SET_SUFFIX)
+numpy.save(TRAIN_DATA_LOCATION + LABELS_FILE_NAME + TRAINING_SET_SUFFIX, y_train)
+print('Training labels saved to:' + TRAIN_DATA_LOCATION + LABELS_FILE_NAME + TRAINING_SET_SUFFIX)
 
 print('Loading test data...')
 X_test = numpy.load(TEST_DATA_LOCATION + EXAMPLES_FILE_NAME)
@@ -34,6 +37,7 @@ y_test = y_test[:TEST_SET_SIZE:1]
 print('Test data: X shape is {}, y shape is {}'.format(X_test.shape, y_test.shape))
 
 print('Saving test data...')
-numpy.save(TEST_DATA_LOCATION + CUT_EXAMPLES_FILE_NAME, X_test)
-numpy.save(TEST_DATA_LOCATION + CUT_LABELS_FILE_NAME, y_test)
-
+numpy.save(TEST_DATA_LOCATION + EXAMPLES_FILE_NAME + TEST_SET_SUFFIX, X_test)
+print('Test examples saved to:' + TEST_DATA_LOCATION + EXAMPLES_FILE_NAME + TEST_SET_SUFFIX)
+numpy.save(TEST_DATA_LOCATION + LABELS_FILE_NAME + TEST_SET_SUFFIX, y_test)
+print('Test labels saved to:' + TEST_DATA_LOCATION + LABELS_FILE_NAME + TEST_SET_SUFFIX)
