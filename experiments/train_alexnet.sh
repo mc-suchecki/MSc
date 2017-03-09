68661a6bc047dede05aeecdd34cb85401788687d
mc-suchecki <mc.suchecki@gmail.com>
2017-03-09 16:13:08 +0100

    Added first version of the neural network in Caffe

---
+++ experiments/train_alexnet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/p307k07/Code/caffe/build/tools/caffe train -solver ./model/solver.prototxt -weights /home/p307k07/Code/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
