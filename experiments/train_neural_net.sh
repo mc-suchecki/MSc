c98678a95e50da97756836909a8dacc24a366824
Maciej Suchecki <mc.suchecki@gmail.com>
2017-04-26 15:16:04 +0200

    Started experiments with ResNet-50

--- experiments/train_neural_net.sh
+++ experiments/train_neural_net.sh
@@ -2,7 +2,8 @@
 
 # log_file_name="./results/alexnet/" # AlexNet
 # log_file_name="./results/oxfordnet/" # OxfordNet
-log_file_name="./results/alexnet-regression/" # AlexNet (regression)
+# log_file_name="./results/alexnet-regression/" # AlexNet (regression)
+log_file_name="./results/resnet-50/" # ResNet
 log_file_name+="`date +%Y-%d-%m-%H-%M-%S`";
 log_file_name+="_log.txt"
 
@@ -11,7 +12,7 @@ log_file_name+="_log.txt"
 # /home/p307k07/Code/caffe/build/tools/caffe train -solver ./model/alexnet/solver.prototxt -weights /home/p307k07/Code/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel &> $log_file_name
 
 # fine-tuning (regression)
-/home/p307k07/Code/caffe/build/tools/caffe train -solver ./model/alexnet-regression/solver.prototxt -weights /home/p307k07/Code/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel &> $log_file_name
+# /home/p307k07/Code/caffe/build/tools/caffe train -solver ./model/alexnet-regression/solver.prototxt -weights /home/p307k07/Code/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel &> $log_file_name
 
 # regular training
 # /home/p307k07/Code/caffe/build/tools/caffe train -solver ./model/alexnet/solver.prototxt &> $log_file_name
@@ -23,3 +24,8 @@ log_file_name+="_log.txt"
 # OxfordNet
 # regular training
 # /home/p307k07/Code/caffe/build/tools/caffe train -solver ./model/oxfordnet/solver.prototxt &> $log_file_name
+
+
+# ResNet-50 fine-tuning
+# ../../caffe/build/tools/caffe train -solver ./model/resnet-50/solver.prototxt -weights ./model/resnet-50/model.caffemodel &> $log_file_name
+
